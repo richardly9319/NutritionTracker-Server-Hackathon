e4b85901898c0b2864feb11e6505654c70617fbe
Richard Ly <richardly9319@gmail.com>
2023-08-11 09:56:40 -0700

    working on axios calls to API

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 8080;
 const fs = require('fs');
 const cors = require('cors');
+const axios = require('axios');
 
-let mealsFile = require('./data/meals.json');
+let mealsDiary = require('./data/mealsDiary.json');
 
 app.use(cors());
 app.use(express.json());
@@ -14,17 +15,31 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    console.log(mealsFile);
+    console.log(mealsDiary);
     console.log("req Body", req.body);
-    fs.readFile('./data/meals.json', 'utf8', function(err, data) {
+    fs.readFile('./data/mealsDiary.json', 'utf8', function(err, data) {
         if (err) {
             console.log(err);
         } else {
-            
+
+            axios.get('https://api.nal.usda.gov/fdc/v1/foods/search?query=cheddar%20cheese&dataType=Foundation,SR%20Legacy&pageSize=1&sortBy=dataType.keyword&sortOrder=asc&api_key=E7kdkQyilTaMxHGab9YFKDxNAqqbQcYymop0A76f').then(
+                response => {
+                    console.log(response.data);
+                }
+                
+            )
+
+
+            // SEND RESPONSE with mealTotalCalories, mealTotalCarbs, mealTotalFats, mealTotalProtein
             obj = JSON.parse(data);
             obj.push(req.body);
-            console.log(obj);
+            // console.log(obj);
             res.send(obj);
+
+            fs.writeFile('mealsDiary.json', JSON.stringify(obj), (error) => {
+                if (error) throw error;
+            });
+
         }
     })
     
