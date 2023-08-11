1c58000b05e64e4a76499f5090fb9098fd973718
Rizwan <rizwan2k10@gmail.com>
2023-08-11 16:52:07 -0700

    Working code to fetch data through axios over usfda api

--- index.js
+++ index.js
@@ -6,94 +6,62 @@ const cors = require('cors');
 const axios = require('axios');
 
 let mealsDiary = require('./data/mealsDiary.json');
-const { Console } = require('console');
-
-app.use(express.json());
-app.use(express.urlencoded());
-// app.use(express.multipart());
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
+// app.use(express.multipart());
 
 app.get('/', (req, res) => {
     res.send("Nutrition Tracker");
 })
 
-const parseIngredientsData = async (ingredient) => {
+function grabNutrientFilter(item) {
+        return {"name": item['nutrientName'], "unit": item['unitName'], "value": item['value']};
+}
 
-        const params={"query": ingredient, "dataType": "Foundation,SR%20Legacy",
-            "pageSize": 1, 
-            "sortBy": "dataType.keyword", 
-            "sortOrder": "asc", 
-            "api_key": "E7kdkQyilTaMxHGab9YFKDxNAqqbQcYymop0A76f"};
+
+const getIngredientsData = async (ingredients) => {
+    let nutriData = [];
+    let urls = [];
+    for (let i = 0; i < ingredients.length; i++) {
+        const params = {
+            "query": ingredients[i]['ingredient'], "dataType": "Foundation,SR%20Legacy",
+            "pageSize": 1,
+            "sortBy": "dataType.keyword",
+            "sortOrder": "asc",
+            "api_key": "E7kdkQyilTaMxHGab9YFKDxNAqqbQcYymop0A76f"
+        };
 
         const myUrlWithParams = new URL("https://api.nal.usda.gov/fdc/v1/foods/search");
         for (const [key, value] of Object.entries(params)) {
             myUrlWithParams.searchParams.append(key, params[key]);
         }
+        urls.push(myUrlWithParams);
+    }
 
-        // API call to server 
-        console.log(myUrlWithParams);
-        return await axios.get(myUrlWithParams).then(
-                (response) => {
-                    console.log('response', response);
-                    console.log(response.data);
-                    return response.data.foods[0].foodNutrients;
-                }   
-        ).catch();
-        // parse data from response
-        // return data
-};
+    const requests = urls.map((url) => axios.get(url));
 
-const getIngredientsData = (ingredients) => {
-    const nutriData = [];
-    console.log("With ingredients: " + ingredients);
-  for(let i = 0; i<ingredients.length; i++) {
-      console.log(ingredients[i]['ingredient']);
-      try {
-        parseIngredientsData(ingredients[i]['ingredient']).then((res) => {
-            console.log(res);
-            nutriData.push(res);
-        }
-        );
-      }
-      catch(event) {
-        console.log("We got", event);
-      }
-  }
-   return Promise.all(nutriData).then((values)=> {console.log(values)});
-//   return ;
-};
-
+    await axios.all(requests).then((responses) => {
+        responses.forEach((resp) => {
+            let msg = {
+                ingredient: resp.data.foods[0].description,
+                info: resp.data.foods[0].foodNutrients.map((item) => grabNutrientFilter(item))
+            };
+            nutriData.push(msg);
+        });
+    });
+    return nutriData;
+}
 
 app.post('/', (req, res) => {
-    console.log(mealsDiary);
+    // console.log(mealsDiary);
     console.log("req Body", req.body);
-    fs.readFile('./data/mealsDiary.json', 'utf8', function(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            const body = req.body;
-            console.log("our Ingredients" + body['ingredients']);
-
-            data = getIngredientsData(body['ingredients']);
-
-
-            console.log(data);
-
-            // SEND RESPONSE with mealTotalCalories, mealTotalCarbs, mealTotalFats, mealTotalProtein
-            obj = JSON.parse(data);
-            obj.push(req.body);
-            // console.log(obj);
-            res.send(obj);
-
-            fs.writeFile('mealsDiary.json', JSON.stringify(obj), (error) => {
-                if (error) throw error;
-            });
-
-        }
-    })
-    
+    const body = req.body;
+    const ingredients = body['ingredients'];
+    getIngredientsData(ingredients).then((result) => {
+        res.json(result)
+    });
 })
 
 app.listen(port, () => {
