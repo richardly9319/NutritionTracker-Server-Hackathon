e8bc88156d054e12b657c683b2903efebb2877f0
anagelberg <amy.nagelberg@gmail.com>
2023-08-12 14:05:11 -0700

    finished hackathon product

--- index.js
+++ index.js
@@ -21,51 +21,12 @@ app.get('/', (req, res) => {
 
 
 const makeUrl = (ingredient) => {
-    myUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${ingredient.replace(' ', '%20')}%20Rice&dataType=Foundation,SR%20Legacy&pageSize=1&sortBy=dataType.keyword&sortOrder=asc&api_key=E7kdkQyilTaMxHGab9YFKDxNAqqbQcYymop0A76f`
+    myUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${ingredient.replace(' ', '%20')}&dataType=Foundation,SR%20Legacy&pageSize=1&sortBy=dataType.keyword&sortOrder=asc&api_key=E7kdkQyilTaMxHGab9YFKDxNAqqbQcYymop0A76f`
 
     return myUrl;
 
 }
-
-
-const parseIngredientsData = async (ingredient) => {
-
-
-    for (const [key, value] of Object.entries(params)) {
-        myUrlWithParams.searchParams.append(key, params[key]);
-    }
-
-    // API call to server 
-    console.log('myUrlWithParams', myUrlWithParams);
-    return await axios.get(myUrlWithParams).then(
-        (response) => {
-            return response.data.foods[0].foodNutrients;
-        }
-    ).catch();
-    // parse data from response
-    // return data
-};
-
-const getIngredientsData = (ingredients) => {
-    const nutriData = [];
-    console.log("With ingredients: " + ingredients);
-    for (let i = 0; i < ingredients.length; i++) {
-        console.log(ingredients[i]['ingredient']);
-        try {
-            parseIngredientsData(ingredients[i]['ingredient']).then((res) => {
-                console.log(res);
-                nutriData.push(res);
-            }
-            );
-        }
-        catch (event) {
-            console.log("We got", event);
-        }
-    }
-    return Promise.all(nutriData).then((values) => { console.log(values) });
-    //   return ;
-};
-
+d
 
 app.post('/', (req, dataRes) => {
 
@@ -73,11 +34,13 @@ app.post('/', (req, dataRes) => {
     let totalFats = 0;
     let totalProtein = 0;
 
+    console.log(req.body.Ingredients);
+
 
     const ingredients = req.body.Ingredients;
     const length = ingredients.length;
     let i = 0;
-
+    console.log(length);
     ingredients.forEach(ingredient => {
         const url = makeUrl(ingredient.ingredient);
 
@@ -99,16 +62,21 @@ app.post('/', (req, dataRes) => {
             totalFats += Fats;
             totalProtein += Protein;
 
+            console.log(Protein);
             i++;
-            if (i === (length - 1)) {
+            if (i === (length)) {
+                console.log('end of array');
                 dataRes.send({
                     protein: totalProtein,
-                    fats: totalFats,
+                    fat: totalFats,
                     carbs: totalCarbs
                 })
             }
 
-        }).catch(err => console.log(err))
+        }).catch(err => {
+            console.log(err)
+            res.status(403);
+        })
     })
 
 })
