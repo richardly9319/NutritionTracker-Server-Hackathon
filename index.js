ea0c9a12f7c38593a4819351e11abfbf1e84a3c8
anagelberg <amy.nagelberg@gmail.com>
2023-09-29 21:48:57 -0700

    add .env for deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+require('dotenv').config();
+const port = process.env.PORT || 5050;
 const fs = require('fs');
 const cors = require('cors');
 const axios = require('axios');
